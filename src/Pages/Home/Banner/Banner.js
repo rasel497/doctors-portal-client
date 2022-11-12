a97087d8310b3f546db0195e7cbb0e4b3ec4da52
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-13 00:33:39 +0600

    create top banner using daisyui hero component

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import chair from '../../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className={`hero lg:h-[500px]`} style={{ backgroundImage: "url('https://i.ibb.co/820xnrr/bg.png')" }}>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="w-1/2 h-80 rounded-lg shadow-2xl" alt='' />
+                <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctorthem: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+          neutral: '#181A2A',
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
